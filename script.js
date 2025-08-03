a1f458a4507e53174b753b7802940ce0d2ecabe5
marcellnw <marcellnw02@gmail.com>
2025-08-03 13:26:59 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-function goToPage2() {
-  document.getElementById("page2").scrollIntoView({ behavior: "smooth" });
-}
-
-// Auto scroll setelah 5 detik
-window.addEventListener("load", () => {
-  setTimeout(goToPage2, 5000);
-});
